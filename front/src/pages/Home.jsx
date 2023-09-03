7e7e80ae2327363f51eb3c57a7346819fdedbe03
aalvaropc <aalvaropc@gmail.com>
2023-09-03 15:16:23 -0500

    add.

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Covid from '../components/Covid/Covid';
 
 function Home() {
   return (
-    <div className='flex items-center justify-center h-96 m-14'>
+    <div className='flex items-center justify-center h-96 m-8'>
       <Covid />
     </div>
   );
